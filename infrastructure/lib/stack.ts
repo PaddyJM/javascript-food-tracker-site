0a919563fa2ae710c70ada8ffe22ebf4219a0f0b
Patrick Morton <patrickjamesmorton@gmail.com>
2022-05-23 17:47:48 +0100

    completed adding crud operation integrations between dynamodb and api agteway

--- infrastructure/lib/stack.ts
+++ infrastructure/lib/stack.ts
@@ -1,7 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as cdk from 'aws-cdk-lib'
-import { Role, ServicePrincipal, Policy, PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam'
+import {
+  Role,
+  ServicePrincipal,
+  Policy,
+  PolicyStatement,
+  Effect,
+} from 'aws-cdk-lib/aws-iam'
 
 export class FoodTrackerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -29,7 +35,7 @@ export class FoodTrackerStack extends Stack {
     const allResources = api.root.addResource(modelName.toLocaleLowerCase())
 
     const oneResource = allResources.addResource('{id}')
-   const deletePolicy = new Policy(this, 'deletePolicy', {
+    const deletePolicy = new Policy(this, 'deletePolicy', {
       statements: [
         new PolicyStatement({
           actions: ['dynamodb:DeleteItem'],
@@ -37,7 +43,7 @@ export class FoodTrackerStack extends Stack {
           resources: [dynamoTable.tableArn],
         }),
       ],
-    });
+    })
 
     const getPolicy = new Policy(this, 'getPolicy', {
       statements: [
@@ -47,7 +53,7 @@ export class FoodTrackerStack extends Stack {
           resources: [dynamoTable.tableArn],
         }),
       ],
-    });
+    })
 
     const putPolicy = new Policy(this, 'putPolicy', {
       statements: [
@@ -57,7 +63,7 @@ export class FoodTrackerStack extends Stack {
           resources: [dynamoTable.tableArn],
         }),
       ],
-    });
+    })
 
     const scanPolicy = new Policy(this, 'scanPolicy', {
       statements: [
@@ -67,26 +73,26 @@ export class FoodTrackerStack extends Stack {
           resources: [dynamoTable.tableArn],
         }),
       ],
-    });
+    })
 
     const deleteRole = new Role(this, 'deleteRole', {
       assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
-    });
-    deleteRole.attachInlinePolicy(deletePolicy);
+    })
+    deleteRole.attachInlinePolicy(deletePolicy)
     const getRole = new Role(this, 'getRole', {
       assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
-    });
-    getRole.attachInlinePolicy(getPolicy);
+    })
+    getRole.attachInlinePolicy(getPolicy)
     const putRole = new Role(this, 'putRole', {
       assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
-    });
-    putRole.attachInlinePolicy(putPolicy);
+    })
+    putRole.attachInlinePolicy(putPolicy)
     const scanRole = new Role(this, 'scanRole', {
       assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
-    });
-    scanRole.attachInlinePolicy(scanPolicy);
+    })
+    scanRole.attachInlinePolicy(scanPolicy)
 
-        const errorResponses = [
+    const errorResponses = [
       {
         selectionPattern: '400',
         statusCode: '400',
@@ -105,13 +111,152 @@ export class FoodTrackerStack extends Stack {
           }`,
         },
       },
-    ];
+    ]
 
     const integrationResponses = [
       {
         statusCode: '200',
       },
       ...errorResponses,
-    ];
+    ]
+
+    const getAllIntegration = new cdk.aws_apigateway.AwsIntegration({
+      action: 'Scan',
+      options: {
+        credentialsRole: scanRole,
+        integrationResponses,
+        requestTemplates: {
+          'application/json': `{
+              "TableName": "${modelName}"
+            }`,
+        },
+      },
+      service: 'dynamodb',
+    })
+
+    const createIntegration = new cdk.aws_apigateway.AwsIntegration({
+      action: 'PutItem',
+      options: {
+        credentialsRole: putRole,
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseTemplates: {
+              'application/json': `{
+                "requestId": "$context.requestId"
+              }`,
+            },
+          },
+          ...errorResponses,
+        ],
+        requestTemplates: {
+          'application/json': `{
+              "Item": {
+                "${modelName}Id": {
+                  "S": "$context.requestId"
+                },
+                "Name": {
+                  "S": "$input.path('$.name')"
+                },
+                "Carbs": {
+                  "N": "$input.path('$.carbs')"
+                },
+                "Protein": {
+                  "N": "$input.path('$.protein')"
+                },
+                "Fat": {
+                  "N": "$input.path('$.fat')"
+                }
+              },
+              "TableName": "${modelName}"
+            }`,
+        },
+      },
+      service: 'dynamodb',
+    })
+
+    const deleteIntegration = new cdk.aws_apigateway.AwsIntegration({
+      action: 'DeleteItem',
+      options: {
+        credentialsRole: deleteRole,
+        integrationResponses,
+        requestTemplates: {
+          'application/json': `{
+              "Key": {
+                "${modelName}Id": {
+                  "S": "$method.request.path.id"
+                }
+              },
+              "TableName": "${modelName}"
+            }`,
+        },
+      },
+      service: 'dynamodb',
+    })
+
+    const getIntegration = new cdk.aws_apigateway.AwsIntegration({
+      action: 'GetItem',
+      options: {
+        credentialsRole: getRole,
+        integrationResponses,
+        requestTemplates: {
+          'application/json': `{
+              "Key": {
+                "${modelName}Id": {
+                  "S": "$method.request.path.id"
+                }
+              },
+              "TableName": "${modelName}"
+            }`,
+        },
+      },
+      service: 'dynamodb',
+    })
+
+    const updateIntegration = new cdk.aws_apigateway.AwsIntegration({
+      action: 'PutItem',
+      options: {
+        credentialsRole: putRole,
+        integrationResponses,
+        requestTemplates: {
+          'application/json': `{
+              "Item": {
+                "${modelName}Id": {
+                  "S": "$method.request.path.id"
+                },
+                "Name": {
+                  "S": "$input.path('$.name')"
+                },
+                "Carbs": {
+                  "N": "$input.path('$.carbs')"
+                },
+                "Protein": {
+                  "N": "$input.path('$.protein')"
+                },
+                "Fat": {
+                  "N": "$input.path('$.fat')"
+                }
+              },
+              "TableName": "${modelName}"
+            }`,
+        },
+      },
+      service: 'dynamodb',
+    })
+
+    const methodOptions = {
+      methodResponses: [
+        { statusCode: '200' },
+        { statusCode: '400' },
+        { statusCode: '500' },
+      ],
+    }
+
+    allResources.addMethod('GET', getAllIntegration, methodOptions)
+    allResources.addMethod('POST', createIntegration, methodOptions)
+
+    oneResource.addMethod('DELETE', deleteIntegration, methodOptions)
+    oneResource.addMethod('GET', getIntegration, methodOptions)
+    oneResource.addMethod('PUT', updateIntegration, methodOptions)
   }
 }
